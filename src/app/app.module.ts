c032a036688129edfd75c5c44bef08eedfbadcc9
buhleqampi <buhle3857@gmail.com>
2024-08-27 15:23:40 +0200

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './educatorSide/navbar/navbar.component';
+import { DashboardComponent } from './educatorSide/dashboard/dashboard.component';
+import { RecordsComponent } from './educatorSide/records/records.component';
+import { RegistrationComponent } from './educatorSide/registration/registration.component';
+import { AnalyticsComponent } from './educatorSide/analytics/analytics.component';
+import { HomeComponent } from './educatorSide/home/home.component';
+import { ProfileComponent } from './studentSide/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    RecordsComponent,
+    RegistrationComponent,
+    AnalyticsComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
