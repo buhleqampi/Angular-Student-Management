f54a8c91b39dbd997c9a8ebcbe334f3a0fec44b3
buhleqampi <buhle3857@gmail.com>
2024-09-03 13:54:44 +0200

    image is not showing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './educatorSide/dashboard/dashboard.component';
+import { RecordsComponent } from './educatorSide/records/records.component';
+import { RegistrationComponent } from './educatorSide/registration/registration.component';
+import { AnalyticsComponent } from './educatorSide/analytics/analytics.component';
+import { HomeComponent } from './educatorSide/home/home.component';
+import { ProfileComponent } from './studentSide/profile/profile.component';
+
+const routes: Routes = [
+  {path: '', redirectTo:'home' , pathMatch: 'full'},
+  {path : 'home', component: HomeComponent},
+  {path : 'dashboard', component: DashboardComponent},
+  {path : 'records', component: RecordsComponent},
+  {path : 'registration', component: RegistrationComponent},
+  {path : 'analytics', component: AnalyticsComponent},
+  {path : 'profile', component: ProfileComponent}, 
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
